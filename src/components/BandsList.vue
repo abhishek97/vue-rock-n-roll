d7b1ab45d99e60730fcc7a3a0df3d427f0a4100b
Abhishek Gupta <hi@itsabhishek.me>
2018-11-13 12:59:47 +0530

    add bandsList and fetch from api

--- src/components/BandsList.vue
+++ src/components/BandsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <input type="text" placeholder="Add Band..">
-    <button> Add </button>
+    <input type="text" placeholder="Add Band.." v-model="newBand" @keyup.enter="addBand">
+    <button @click="addBand"> Add </button>
 
     <ul>
       <BandItem v-for="band in bands" :key="band.id" :band="band">
@@ -20,8 +20,16 @@ export default {
   components: {
     BandItem
   },
+  created () {
+    fetch('https://my-json-server.typicode.com/abhishek97/json/bands')
+      .then(res => res.json())
+      .then(res => {
+        this.bands = res
+      })
+  },
   data () {
     return {
+      newBand: '',
       bands: [{
         id: 1,
         name: 'Black Sabbath',
@@ -31,6 +39,16 @@ export default {
         name: 'ColdPlay',
         songs: ['Yellow', 'Fix you', 'Mylo xyloto']
       }]
+    }
+  },
+  methods: {
+    addBand () {
+      this.bands.push({
+        id: Math.floor(Math.random() * 100),
+        name: this.newBand,
+        songs: []
+      })
+      this.newBand = ''
     }
   }
 }
